f9da6870414c89b4e3e3aec92ecb38b305d1cf41
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2022-10-08 01:57:15 +0900

    team_project 모델 생성

--- team_project/models.py
+++ team_project/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#팀 프로젝트 메인 페이지
+class Project(models.Model):
+    #팀 모델과 일대일 관계
+    #band_id = models.OneToOneField(team.Team, on_delete=models.CASCADE) 추후 작성
+    title = models.CharField(max_length=30)
+    date_start = models.DateField()
+    date_end = models.DateField()
+    introduce = models.TextField()
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
