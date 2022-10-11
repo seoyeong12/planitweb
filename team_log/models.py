f224a4a1b9506596b8aae08850bca3ab97ed4142
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2022-10-11 20:45:44 +0900

    모델 url 등록 및 html 연결(FBV)

--- team_log/models.py
+++ team_log/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 # Create your models here.
+import team_project.models
 
 class Log(models.Model):
     # 팀 프로젝트와 다대일 관계(하나의 팀은 여러 회의 노트를 가질 수 있다.)
-    # note_id = models.ForeignKey(team_project.models.Project, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
+    team = models.ForeignKey(team_project.models.Project, on_delete=models.CASCADE, null=True)
     state = models.BooleanField(default=True)
+    title = models.CharField(max_length=50)
 
     #나중에 views.py 로 옮기기
     def __str__(self):
--- team_project/urls.py
+++ team_project/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-#crete_team
 urlpatterns = [
     path('', views.index),#프로젝트 모아보기
     path('team_detail', views.detail)#프로젝트 상세 보기..? 메인 디자인에 따라서 달라짐
