ea22ea0fbb18bbbadb300373eeb480070d7c9b36
rain_hyeon <iiilee0907@naver.com>
2022-10-26 01:38:19 +0900

    gg

--- user/models.py
+++ user/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Team(models.Model):
-    team_name = models.CharField(max_length=30, default="")
 class User(models.Model) :
-    team = models.ForeignKey(Team, on_delete=models.SET_NULL,null=True)
     user_name = models.CharField(max_length=10, default="")
     email = models.EmailField(default="")
     password = models.CharField(max_length=50, default="", verbose_name="비밀번호")
