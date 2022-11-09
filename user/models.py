6a74086cb7faacd85209e9cc261305d3e64f17c2
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2022-11-10 00:48:26 +0900

    user 충돌 및 업데이트

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model) :
-    
+
     team = models.ForeignKey('team_project.Team', on_delete=models.SET_NULL, null=True, blank=True, default=None)
     user_name = models.CharField(max_length=10, default="")
     email = models.EmailField(default="")
